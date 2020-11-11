b2a1d0219095c8fa97123d75ad6c0e22fcaad3a2
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-10 22:51:27 -0500

    index data area complete, for now

--- src/components/DataArea/index.js
+++ src/components/DataArea/index.js
@@ -83,3 +83,20 @@ const DataArea = () => {
           });
         });
       }, []);
+
+      return (
+        <DataAreaContext.Provider
+          value={{ developerState, handleSearchChange, handleSort }}
+        >
+          <Nav />
+          <div className="data-area">
+            {developerState.filteredUsers.length > 0 
+    ? <DataTable />
+     : <div></div>
+     }
+          </div>
+        </DataAreaContext.Provider>
+      );
+    }
+    
+    export default DataArea; 
