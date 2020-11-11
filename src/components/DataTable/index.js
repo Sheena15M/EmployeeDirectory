10464c34f2191639f3d1ce40f6815710bb8a82ee
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-11 10:11:36 -0500

    import components added to index for datatable

---
+++ src/components/DataTable/index.js
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import DataBody from "../DataBody";
+import "./DataTable.css";
+import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataTable = () => {
+    const context = useContext(DataAreaContext);
