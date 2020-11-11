7eaa817e7d3ffc919941e09245a50b0c065e00a5
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-11 10:07:02 -0500

    info line for directory

--- src/components/DataBody/index.js
+++ src/components/DataBody/index.js
@@ -27,3 +27,28 @@ const DataBody = () => {
                     className="img-responsive"
                   />
                 </td>
+                <td data-th="Name" className="name-cell align-middle">
+                  {name.first} {name.last}
+                </td>
+                <td data-th="Phone" className="align-middle">
+                  {phone}
+                </td>
+                <td data-th="Email" className="align-middle">
+                  <a href={"mailto:" + email} target="__blank">
+                    {email}
+                  </a>
+                </td>
+                <td data-th="DOB" className="align-middle">
+                  {formatDate(dob.date)}
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </tbody>
+    );
+  }
+  
+  export default DataBody;
