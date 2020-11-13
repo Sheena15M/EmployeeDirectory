81fcc33a38dcb539aee9fa84a07100ae50e18dfa
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-12 19:40:06 -0500

    index.js for header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Search for an employee via the filter or the search bar. Happy Hunting</p>
+        </div>
+    )
+}
+
+export default Header;
